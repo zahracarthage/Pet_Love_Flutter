768bec9e189392d2a03391fbf6f0ec5f1c3ec751
zahra chouchane <zahracarthage.chouchene@esprit.tn>
2022-06-05 11:44:11 +0100

    initial commit + color const

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'dart:ui';
+
+const Color primaryOrange = Color.fromRGBO(252, 151, 49, 100);
+const Color primaryPink = Color.fromRGBO(254, 68, 97, 100);
+const Color gradientStart = Color.fromRGBO(253, 152, 53, 100);
+const Color gradientEnd = Color.fromRGBO(255, 210, 120, 100);
